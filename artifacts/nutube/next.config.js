8078b7a5d7fabf14495c22ab4b9d2297f0a6da14
apark12321-ux <apark12321@gmail.com>
2026-05-18 19:04:33 +0900

    Update next.config.js

--- artifacts/nutube/next.config.js
+++ artifacts/nutube/next.config.js
@@ -5,8 +5,6 @@ const nextConfig = {
     remotePatterns: [],
     unoptimized: true,
   },
-  // 박 대표님 옛 코드와 @types/react 버전 충돌 회피
-  // 컴파일은 정상 작동, 타입 체크 단계만 우회
   typescript: {
     ignoreBuildErrors: true,
   },
